eb4d4d7e2c8897b50e9cffb4eeb4022e926ccd74
sanjiyip <sanjiyip@hotmail.com>
2017-11-10 13:56:14 +0800

    11-10

--- Playground/playground.js
+++ Playground/playground.js
@@ -1,118 +1,41 @@
-function foo() {
-    console.log(this.a);
-}
-a = 4;
-var obj = {
-    a: 2,
-    foo: foo
-};
-obj.foo();
-
-
------
-
-function foo() {
-    console.log(this.a);
-}
-var obj = {
-    a: 2,
-    foo: foo
-};
-
-
-var a = "oops,global";
-
-obj.foo();
-
-console.log(bar);
-
-bar();
-------
-
-"use strict"
-function foo() {
-    console.log(this.a);
-}
-
-function doFoo(fn) {
-    fn();
-}
-
-var obj = {
-    a: 2,
-    foo: foo
-}
-
-var a = "oops,global";
-doFoo(obj.foo);
-
-function identify() {
-    return this.name.toUpperCase();
-}
-
-function speak() {
-    var greeting = "Hi,I'm "+identify.call(this);
-    console.log(greeting);
-}
-
-var me = {
-    name: "keyle"
-};
-
-var you = {
-    name: "Reader"
-};
-
-identify.call(me);
-identify.call(you);
-speak.call(me);
-speak.call(you);
-
-function foo(num) {
-    console.log("foo:" + num);
-    this.count++;
-}
-
-foo.count = 0;
-
-for(var i=0; i<10; i++) {
-    if(i>5) {
-        foo.call(foo, i);
+function bubble(arr, n) {
+    // n 表示数组长度~
+    for (var i = 0; i < n - 1; i++) {
+        var temp;
+        if (arr[i] > arr[i + 1]) {
+            temp = arr[i];
+            arr[i] = arr[i + 1];
+            arr[i + 1] = temp;
+        }
     }
 }
 
-console.log(foo.count);
+function bubbleSort_Baby(arr, n) {
+    // n 为数组长度
 
+    // 对前面 n 个数进行一次冒泡，最大值找到，放到最末尾
+    bubble(arr, n);
 
----
+    // 对前面 n-1 个数进行一次冒泡，第二大值找到，放到倒数第二位
+    bubble(arr, n - 1);
 
+    // 对前面 n-2 个数进行一次冒泡，...
+    bubble(arr, n - 2);
 
-function foo() {
-    console.log(this.a);
+    //...
+    //...  不断执行冒泡这个步骤
+    //...
+
+    // 对前面 2 个数进行一次冒泡
+    bubble(arr, 2);
+
+    // 对前面 1 个数进行一次冒泡
+    bubble(arr, 1);
+
+    //返回数组
+    return arr;
 }
 
-var obj = {
-    a: 2
-};
+var array = [14,3,8,1,23,6];
 
-
-
-var a = {x:1, y:2};
-function Foo(o) {
-    o.x = "changed";
-    o = [1, 2, 3];
-    o.y = "can I change?";
-}
-
-Foo(a);             // 将对象的引用地址的复本传递到函数的形参中
-
-console.log(a.x);   // "changed" 被改变了
-console.log(a.y);   // 2 对函数里面的形参重新赋值后,原对象(实参)不再受形参的影响
-
-var a = [1,2,3];
-var b = a;
-b.push(4);  //[1,2,3,4]
-a;
-b = ["a", "b", "c"];
-b.slice(1);
-a;
+bubbleSort_Baby(array, 6);
